a0ef07b76cfe1de3bc16f7495fe127bb048b4a34
Robert Geifman <robertgeifman@me.com>
2023-11-26 19:45:18 +0100

    Initial Commit

---
+++ AXMenu/AXMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  AXMenuApp.swift
+//  AXMenu
+//
+//  Created by Robert Geifman on 26/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AXMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
